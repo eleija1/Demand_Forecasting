205181df9ff49b5cd0240053dd7bb9d90e372a2a
eleija1 <133179691+eleija1@users.noreply.github.com>
2026-04-13 21:16:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Store Sales Predictor", page_icon="📦")
 st.title("📦 Store Sales Predictor")
-
+st.write("Enter product and outlet information to estimate item outlet sales.")
 
 
 @st.cache_resource
